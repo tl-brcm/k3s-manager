354c861fd0624b46b0d91ebbe0270d8dca0b9711
Tony Liang <tony.liang@broadcom.com>
2024-01-10 21:48:17 +0800

    added external connection ip address

---
+++ script/sh/configure-k3s-client.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Source utility scripts
+source modules/K3sUtils.sh
+source modules/MultipassUtils.sh
+source modules/LoggingUtils.sh
+
+scriptBaseDir=$(dirname "$(realpath "$0")")
+
+# Check if prefix is provided
+if [ -z "$1" ]; then
+    # Get the first available directory under slaves if no prefix is provided
+    slave_dir=$(find "$scriptBaseDir/../../config/slaves/" -mindepth 1 -maxdepth 1 -type d | head -n 1)
+    if [ -z "$slave_dir" ]; then
+        write_log "No slave configuration directory found." "ERROR"
+        exit 1
+    fi
+    write_log "No prefix provided. Using configuration from $slave_dir" "WARN"
+else
+    slave_dir="$scriptBaseDir/../../config/slaves/$1"
+fi
+
+configPath="$slave_dir/config.json"
+hostListPath="$slave_dir/host_list"
+
+# Load configuration from JSON file
+if [ ! -f "$configPath" ]; then
+    write_log "Config file not found at $configPath" "ERROR"
+    exit 1
+fi
+
+configJson=$(cat "$configPath")
+
+# Extract VM settings from the config
+vmName=$(echo "$configJson" | jq -r '.vmSettings.client.vmName')
+cpuCores=$(echo "$configJson" | jq -r '.vmSettings.client.cpuCores')
+ram=$(echo "$configJson" | jq -r '.vmSettings.client.ram')
+disk=$(echo "$configJson" | jq -r '.vmSettings.client.disk')
+vmIp=$(echo "$configJson" | jq -r '.vmSettings.client.vmIp')
+
+# Other configuration
+timeoutSeconds=$(echo "$configJson" | jq -r '.timeoutSeconds')
+masterVmName=$(echo "$configJson" | jq -r '.vmSettings.master.vmName') # Assuming the master's name is still relevant for the slave setup
+k3sConfigFile="k3s.yaml"
+localTempPath="$scriptBaseDir/temp/"
+localScriptPath="$scriptBaseDir/install_kubectx_kubens.sh"
+remoteTempPath="/home/ubuntu/"
+use_scp=true
+
+
+# Copy K3s Config
+write_log "Copying K3s configuration from $masterVmName to $vmName..." "INFO"
+copy_k3s_config "$masterVmName" "$vmName" "$k3sConfigFile" "$localTempPath" "$remoteTempPath" $use_scp
+
+
+write_log "$vmName VM setup completed successfully" "INFO"
--- script/sh/modules/K3sUtils.sh
+++ script/sh/modules/K3sUtils.sh
@@ -126,14 +126,16 @@ install_k3s_worker() {
     temp_script="temp/install_k3s_$vm_name.sh"
     
     printf '#!/bin/bash\n' > "$temp_script"
-    printf "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-master:6443 K3S_TOKEN=$k3s_token INSTALL_K3S_VERSION='v1.27.8+k3s2' K3S_NODE_IP='$k3s_node_ip' sh -s - --node-ip $k3s_node_ip" >> "$temp_script"
+    printf "curl -sfL https://get.k3s.io | K3S_URL=https://k3s-master:6443 K3S_TOKEN=$k3s_token INSTALL_K3S_VERSION='v1.27.8+k3s2' sh -s - --node-external-ip=$k3s_node_ip" >> "$temp_script"
     printf '\n' >> "$temp_script"
 
     multipass transfer "$temp_script" "$vm_name:/home/ubuntu/install_k3s.sh"
     # Execute the script
     multipass exec "$vm_name" -- chmod +x /home/ubuntu/install_k3s.sh    
-    sleep 20
-    write_log "Wait for 20 secs for the VM to get ready..."
+    write_log "Wait for 30 secs for the VM to get ready..."
+    sleep 30
+    multipass exec "$vm_name" -- sudo apt-get update
+    multipass exec "$vm_name" -- sudo sudo apt install -y wireguard
     multipass exec "$vm_name" -- bash -c "/home/ubuntu/install_k3s.sh"
 
     # Remove the local temp script file
